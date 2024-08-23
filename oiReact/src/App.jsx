36f349946c57388c559cec26207e1ae23dc4a794
naiara_r_silva@estudante.sesisenai.org.br <naiara_r_silva@estudante.sesisenai.org.br>
2024-08-23 17:16:13 -0300

    criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Dinossauros</h1> 
+     <h1>Carnivoros</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit
+  . At consectetur magni nam obcaecati, eum harum? Aliquid, corporis iure! 
+  Sit exercitationem aspernatur dolor libero.
+  Possimus alias id, ipsam adipisci repudiandae eum?</p>
+    </>
+  )
+}
+
+export default App
